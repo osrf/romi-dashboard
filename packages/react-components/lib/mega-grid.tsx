99b50134b1811f322011a4e739f8d2fcbb8597fa
Morgan Quigley <morgan@osrfoundation.org>
2021-01-11 16:31:52 +0800

    experimenting with react-grid-layout
    Signed-off-by: Morgan Quigley <morgan@osrfoundation.org>

--- packages/react-components/lib/mega-grid.tsx
+++ packages/react-components/lib/mega-grid.tsx
@@ -1,27 +1,39 @@
+//import RGL, { WidthProvider } from 'react-grid-layout';
+const ReactGridLayout = require('react-grid-layout');
+import '../node_modules/react-grid-layout/css/styles.css';
+import '../node_modules/react-resizable/css/styles.css';
+import './mega-grid.css';
+
 import React, { Component } from 'react';
-import RGL, { WidthProvider } from 'react-grid-layout';
 
-class MegaGrid extends React.Component {
+export class MegaGrid extends React.Component {
   render() {
-    return <h1>foo5</h1>;
-  }
-}
-
-export default MegaGrid;
-
-// layout is an array of objects, see the demo for more complete usage
-/*
     const layout = [
       { i: 'a', x: 0, y: 0, w: 1, h: 2, static: true },
-      { i: 'b', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
+      { i: 'b', x: 1, y: 1, w: 3, h: 2, minW: 2, maxW: 4 },
       { i: 'c', x: 4, y: 0, w: 1, h: 2 },
     ];
     return (
-      <GridLayout className="layout" layout={layout} cols={12} rowHeight={30} width={1200}>
-        <div key="a">a</div>
-        <div key="b">b</div>
-        <div key="c">c</div>
-      </GridLayout>
+      <div className="container">
+        <ReactGridLayout
+          className="layout"
+          layout={layout}
+          cols={12}
+          rowHeight={30}
+          width={1200}
+          compactType={null}
+        >
+          <div key="a">
+            <span className="text">A is not draggable</span>
+          </div>
+          <div key="b">
+            <span className="text">B is draggable and resizable</span>
+          </div>
+          <div key="c">
+            <span className="text">C is draggable and resizable</span>
+          </div>
+        </ReactGridLayout>
+      </div>
     );
-    */
-//}
+  }
+}
